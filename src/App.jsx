cea667ed215b13af426283dce26060f43149e348
Ishan <ishan.dhingra.work@gmail.com>
2025-04-08 04:19:14 +0530

    feat: add preload page for initial app loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Preloader } from "./components";
+import { AnimatePresence } from "motion/react";
 
 const App = () => {
-  return <div>App</div>;
+  const [isLoading, SetIsLoading] = useState(true);
+
+  setTimeout(() => {
+    SetIsLoading(false);
+  }, 3000);
+
+  return (
+    <main>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/Preloader.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { motion } from "motion/react";
+
+const Preloader = () => {
+  const slideUp = {
+    initial: {
+      y: "0",
+    },
+    exit: {
+      y: "-100vh",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 },
+    },
+  };
+
+  return (
+    <motion.div
+      variants={slideUp}
+      initial="initial"
+      exit="exit"
+      className="bg-[#3a2e2e] flex justify-center items-center h-screen overflow-hidden"
+    >
+      <div className="teacup-area">
+        <div className="steam steam1"></div>
+        <div className="steam steam2"></div>
+        <div className="teacup">
+          <div className="chai-liquid-level"></div>
+          <div className="bubble bubble1"></div>
+          <div className="bubble bubble2"></div>
+          <div className="bubble bubble3"></div>
+        </div>
+      </div>
+      <div className="saucer"></div>
+
+      <p className="font-DM-Serif-Text font-bold text-gray-600 tracking-wider text-[min(4vw,40px)] relative top-10">
+        “Brewing knowledge & chai... just a sip away.”
+      </p>
+    </motion.div>
+  );
+};
+
+export default Preloader;
